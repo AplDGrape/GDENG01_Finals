7d5a4187830385d6740b5ba11392b0926b697212
Kenneth2Shu <darkryder42@gmail.com>
2023-11-17 14:53:05 +0800

    added mats, textures, and references

---
+++ Assets/Scripts/AssetReferences.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetReferences : MonoBehaviour
+{
+    /*
+    Rocks: https://free3d.com/3d-model/low-poly-rock-set-5-different-rocks-92678.html
+
+    Grass Texture + Land: https://free3d.com/3d-model/-rectangular-grass-patch--205749.html
+
+    Wood floor deck: https://polyhaven.com/a/wood_floor_deck
+
+    Plank flooring: https://polyhaven.com/a/plank_flooring
+
+    Moss wood: https://polyhaven.com/a/moss_wood
+
+    Leafy grass: https://polyhaven.com/a/leafy_grass
+
+    Forest ground: https://polyhaven.com/a/forrest_ground_01
+
+    Bark brown 1: https://polyhaven.com/a/bark_brown_01
+
+    Bark brown 2: https://polyhaven.com/a/bark_brown_02
+    */
+}
