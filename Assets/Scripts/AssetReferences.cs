04dbdd9e257dae6abcdc1aa5af05480a61740efe
Kenneth2Shu <darkryder42@gmail.com>
2023-11-24 14:09:40 +0800

    reference url for leaves

--- Assets/Scripts/AssetReferences.cs
+++ Assets/Scripts/AssetReferences.cs
@@ -22,5 +22,11 @@ public class AssetReferences : MonoBehaviour
     Bark brown 1: https://polyhaven.com/a/bark_brown_01
 
     Bark brown 2: https://polyhaven.com/a/bark_brown_02
+
+    Leaf 1: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.ph%2Fpin%2Fgreen-leaf-png-clip-art--506232814350111830%2F&psig=AOvVaw0Nt47bHuWlYDBoKg1p9Sh_&ust=1700891666941000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCLjO867524IDFQAAAAAdAAAAABAh
+
+    Leaf 2: https://www.kindpng.com/imgv/TJRoibx_peach-leaf-clipart-hd-png-download/
+
+    Leaf 3: https://www.kindpng.com/imgv/TJRoibR_leaf-clipart-illustration-hd-png-download/
     */
 }
