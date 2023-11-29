df57bb1c383eb5c6098532bb7360691baaead077
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2023-11-29 19:20:00 +0800

    Imported doors and updated my asset sources :)

--- Assets/Scripts/AssetReferences.cs
+++ Assets/Scripts/AssetReferences.cs
@@ -23,6 +23,26 @@ public class AssetReferences : MonoBehaviour
 
     Bark brown 2: https://polyhaven.com/a/bark_brown_02
 
+    Beige wall: https://polyhaven.com/a/beige_wall_001
+
+    Fined Grained Wood: https://polyhaven.com/a/fine_grained_wood
+
+    Brown Planks 08: https://polyhaven.com/a/brown_planks_08
+
+    Coast sand 05: https://polyhaven.com/a/coast_sand_05
+
+    Dark wood: https://polyhaven.com/a/dark_wood
+
+    Pavement 03: https://polyhaven.com/a/pavement_03
+
+    Wood planks: https://polyhaven.com/a/wood_planks
+
+    Wood table worn: https://polyhaven.com/a/wood_table_worn
+
+    Classic Interior Door Pack 1 by Jan Fidler: https://assetstore.unity.com/packages/3d/props/interior/classic-interior-door-pack-1-118744
+
+
+
     Leaf 1: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.ph%2Fpin%2Fgreen-leaf-png-clip-art--506232814350111830%2F&psig=AOvVaw0Nt47bHuWlYDBoKg1p9Sh_&ust=1700891666941000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCLjO867524IDFQAAAAAdAAAAABAh
 
     Leaf 2: https://www.kindpng.com/imgv/TJRoibx_peach-leaf-clipart-hd-png-download/
