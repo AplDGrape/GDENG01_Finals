b1c76d776272dda2a5913b8af78f30edc35b370e
AplDGrape <61457409+AplDGrape@users.noreply.github.com>
2023-11-30 19:22:29 +0800

    imported more wooden furniture assets & updated my references

--- Assets/Scripts/AssetReferences.cs
+++ Assets/Scripts/AssetReferences.cs
@@ -43,6 +43,20 @@ public class AssetReferences : MonoBehaviour
 
     Synthetic Wood: https://polyhaven.com/a/synthetic_wood
 
+    Palm tree bark: https://polyhaven.com/a/palm_tree_bark
+
+    Brick wall 005: https://polyhaven.com/a/brick_wall_005
+
+    Bark willow: https://polyhaven.com/a/bark_willow
+    
+    Wooden english antique chair: https://assetstore.unity.com/packages/3d/props/wooden-english-antique-chair-266466
+
+    Old wooden cabinet: https://assetstore.unity.com/packages/3d/props/furniture/old-wooden-cabinet-106249
+
+    Raw wooden furniture: https://assetstore.unity.com/packages/3d/props/furniture/raw-wooden-furniture-free-166329
+
+
+
     Leaf 1: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.ph%2Fpin%2Fgreen-leaf-png-clip-art--506232814350111830%2F&psig=AOvVaw0Nt47bHuWlYDBoKg1p9Sh_&ust=1700891666941000&source=images&cd=vfe&opi=89978449&ved=0CBQQjhxqFwoTCLjO867524IDFQAAAAAdAAAAABAh
 
     Leaf 2: https://www.kindpng.com/imgv/TJRoibx_peach-leaf-clipart-hd-png-download/
